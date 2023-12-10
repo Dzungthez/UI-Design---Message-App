b49cf4ba54684213ecb03af65f8d6d4dff2f986e
Dzzzung <huudung141434@gmail.com>
2023-12-10 22:13:42 +0700

    first and last

---
+++ README.md
@@ -0,0 +1,3 @@
+# Design UI for message app
+## Demo
+<img src="386881667_3680973398790839_6569999563151499523_n.png"/>
---
+++ src/main/java/ui/AppUI.java
@@ -0,0 +1,29 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppUI extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(AppUI.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Messenger App");
+        // change color of title bar to black
+
+        stage.setResizable(false); // không cho phóng to, thu nhỏ cửa sổ
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
